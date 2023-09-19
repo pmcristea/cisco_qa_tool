7308cd46466d6303988489be0dbecf30894f1aa9
papir805 <papir805@gmail.com>
2023-09-18 19:47:41 -0700

    Initial commit

---
+++ cisco_qa_tool_notebook.py
@@ -0,0 +1,25 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.15.1
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %%
+from requests_html import HTMLResponse
+
+from dotenv import find_dotenv, load_dotenv
+
+import os
+
+import langchain
+
+# %%
